23d6a4792d6a1bf4f597a7030ccacb3b5df95a8d
Jason Schmidt <jsn@visualplant.net>
2012-08-10 17:33:19 -0700

    First add
    Adding Objectlab's splash page for the first time

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!-- /ht Paul Irish - http://front.ie/j5OMXi -->
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<title>objectlab</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<meta http-equiv="cleartype" content="on">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+<link rel="shortcut icon" href="/link/to/favicon.ico">
+<link rel="apple-touch-icon" href="/link/to/apple-touch-icon.png">
+
+<!-- Adaptive Images - including for retina screens - must go above all other JS file calls -->
+<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
+
+<link rel="stylesheet" href="css/style.css" media="all">
+<link rel="stylesheet" href="css/fonts.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="css/icons.css" />
+<!--[if lte IE 7]><script src="js/libs/lte-ie7.js"></script><![endif]-->
+
+<!--[if (lt IE 9) & (!IEMobile)]>
+<link rel="stylesheet" href="css/layout.css" media="all">
+<![endif]-->
+
+<script src="js/libs/modernizr-1.7.min.js"></script>
+
+<!-- iOS -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="apple-touch-startup-image" href="images/splash.png">
+  
+</head>
+
+<body>
+
+<div id="page">
+
+	<div class="logo"></div>
+
+	<div class="tagline">
+		<span class="skill">Technology</span> <span class="bullet-blue" aria-hidden="true" data-icon="&#x22;"></span> 
+		<span class="skill">Strategy</span> <span class="bullet-blue" aria-hidden="true" data-icon="&#x22;"></span>
+		<span class="skill">Implementation</span>
+	</div>
+	
+	<div class="cities">
+		<span class="city">New York</span> <span class="bullet-green" aria-hidden="true" data-icon="&#x22;"></span>
+		<span class="city">Princeton</span> <span class="bullet-green" aria-hidden="true" data-icon="&#x22;"></span>
+		<span class="city">Austin</span> <span class="bullet-green" aria-hidden="true" data-icon="&#x22;"></span>
+		Silicon Valley
+	</div>
+	
+	<img src="images/black-rule.png" class="rule">
+	
+	<div onclick="location.href='mailto:cmollis@objectlab.com?cc=craigm@objectlab.com'" class="button">
+		Get in Touch
+	</div>
+	
+	<div class="social">
+		<span onclick="location.href='https://twitter.com/#!/objectlabtwits'" style="cursor:pointer;font-size:2.6em;color:#ccc;margin:3px;" aria-hidden="true" data-icon="&#x25;"></span>
+		<span onclick="location.href='http://www.linkedin.com/company/objectlab'" style="cursor:pointer;font-size:2.6em;color:#ccc;margin:3px;" aria-hidden="true" data-icon="&#x28;"></span>
+		<span onclick="location.href='http://objectblab.wordpress.com'" style="cursor:pointer;font-size:2.6em;color:#ccc;margin:3px;" aria-hidden="true" data-icon="&#x24;"></span>
+	</div>
+	
+</div>
+
+<script type="text/javascript" src="js/libs/retina.js"></script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33170871-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  
+</body>
+
+</html>
